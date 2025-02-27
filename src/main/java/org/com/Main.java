3e4ce73c431aca93ede50073ba82028ac9b3b0d3
dotae1 <9636515@gmail.com>
2025-02-27 17:21:16 +0900

    programmer today

---
+++ src/main/java/org/com/Main.java
@@ -0,0 +1,14 @@
+package org.com;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       int i=3;
+       int a = 5;
+        System.out.println(i/2);
+        System.out.println(a/2);
+
+
+    }
+}
---
+++ src/main/java/org/com/programmers_03.java
@@ -0,0 +1,38 @@
+package org.com;
+
+public class programmers_03 {
+    public static void main(String[] args) {
+        String s = "1234";
+        boolean answer = true;
+            int len = s.length();
+
+            if(len==4 || len==6){
+                for(int i=0;i<len;i++){
+                    char c = s.charAt(i);
+                    if((c>=65 && c<=90) || (c >=97 && c<=122)){
+                        answer = false;
+                        break;
+                    }
+                }
+            } else {
+                answer = false;
+            }
+
+        System.out.println(answer);
+        }
+    }
+
+    /* 신박한 방법
+    public boolean solution(String s) {
+      if(s.length() == 4 || s.length() == 6){
+          try{
+              int x = Integer.parseInt(s);
+              return true;
+          } catch(NumberFormatException e){
+              return false;
+          }
+      }
+      else return false;
+  }
+     */
+
