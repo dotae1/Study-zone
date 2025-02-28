c178669fd07a881aca69fa96de6f6eaab7e2ae86
dotae1 <9636515@gmail.com>
2025-02-28 17:49:33 +0900

    code programmer

---
+++ src/main/java/org/com/programmers_04.java
@@ -0,0 +1,14 @@
+package org.com;
+
+public class programmers_04 {
+    public static void main(String[] args) {
+        int n=50;
+        int answer = 0;
+        for(int i=1; i<=n;i++){
+            if(n%i==0){
+                answer += i;
+            }
+        }
+        System.out.println(answer);
+    }
+}
---
+++ src/main/java/org/com/programmers_05.java
@@ -0,0 +1,26 @@
+package org.com;
+
+public class programmers_05 {
+    public static void main(String[] args) {
+            boolean answer = true;
+            int x = 10;
+            int sum = 0;
+            String str = Integer.toString(x); //x값을 String형식으로 변환
+
+            for(int i=0;i<str.length();i++){
+                char c = str.charAt(i); //String str의 하나하나 c에 저장
+                sum += c -  '0'; // 문자 c는 - '0'을 하면 ASCII CODE값에 의해 원래 값으로 돌아옴
+            }
+
+            if(x%sum ==0){
+                answer = true;
+            }else {
+                answer = false;
+            }
+
+            System.out.println(answer);
+        }
+    }
+
+
+
