9c7864188eaed4551efd6505a04daba2c39cb697
dotae1 <9636515@gmail.com>
2025-03-03 22:25:56 +0900

    1day 1commit

--- src/main/java/org/com/programmers/programmers_08.java
+++ src/main/java/org/com/programmers/programmers_08.java
@@ -1,25 +1,34 @@
 package org.com.programmers;
+
 import java.util.*;
 
 public class programmers_08 {
-    public static void main(String[] args) {
-        int answer = 0;
-        int n = 9123;
-        ArrayList<Integer> numList = new ArrayList<>();
+    public static void main(String args[]) {
+        boolean answer = true;
+        int co = 0;
+        int co1 = 0;
+        String s = "pyPPYy";
 
-        // 숫자의 각 자리수를 ArrayList에 저장
-        while (n > 0) {
-            numList.add(n % 10); // 마지막 자리 숫자를 추가
-            n /= 10; // 마지막 자리 제거
+
+        // String을 char 배열로 변환
+        char[] charArray = s.toCharArray();
+
+        // for-each 문을 사용하여 char 배열을 순회
+        for (char a : charArray) {
+            if (a == 'p' || a == 'P') {  // 대소문자 처리 추가
+                co += 1;
+            } else if (a == 'y' || a == 'Y') {  // 대소문자 처리 추가
+                co1 += 1;
+            }
         }
 
-        // 저장된 숫자들을 모두 더하기
-        for (int num : numList) {
-            answer += num;
+        // p와 y의 개수 비교
+        if (co == co1) {
+            answer = true;
+        } else {
+            answer = false;
         }
 
         System.out.println(answer);
     }
 }
-
-
