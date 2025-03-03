f9b75b13ee16116f3da6b71578cbecff85cbe410
dotae1 <9636515@gmail.com>
2025-03-03 23:46:04 +0900

    1day 1commit

--- src/main/java/org/com/mission/java_mission02.java
+++ src/main/java/org/com/mission/java_mission02.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 public class java_mission02 {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력하세요 : ");
+        System.out.print("두 정수를 입력하세요 : ");
         int a = scr.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요 : ");
         int b = scr.nextInt();
 
         if(a>b){
